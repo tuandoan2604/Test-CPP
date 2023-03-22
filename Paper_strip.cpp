e2ab2e470efa140ec459fd9d0df98f8f6180389b
nganngan0212 <ngan.uet@gmail.com>
2023-03-22 15:59:32 +0700

    bai 2

---
+++ Paper_strip.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int minPieces(const vector<int>& original, const vector<int>& desired)
+{
+    // throw std::logic_error("Waiting to be implemented");
+
+    int cut = 0;
+
+    for(int i=0; i<original.size();)
+    {
+        for(int j=0; j<desired.size();j++)
+        {
+            if(original[i]==desired[j])
+            {
+                i++;
+                for (int k=j+1; k<desired.size(); k++)
+                {
+                    if(original[i]==desired[k])
+                    {
+                        i++;
+                        continue;
+                    }
+                    else{
+                        cut++;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+    return cut;
+}
+
+// #ifndef RunTests
+int main()
+{
+    // vector<int> original = { 1, 4, 3, 2, 10, 5, 6, 13};
+    // vector<int> desired = { 1, 2, 10, 5, 4, 3, 13, 6 };
+
+    vector<int> original = { 1, 4, 3, 5, 2, 10};
+    vector<int> desired = { 1, 2, 10, 5, 4, 3};
+
+    int num = minPieces(original, desired);
+    cout << num << endl;
+
+    return 0;
+}
+// #endif
+
+
+
