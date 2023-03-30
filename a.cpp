cb127cab1dbb6b567108407689ff8c071c534b7c
Đức Ngọc <tk632k@gmail.com>
2023-03-30 22:20:58 +0700

    them bai 2

---
+++ a.cpp
@@ -0,0 +1,23 @@
+
+// C++ code to demonstrate copy of vector 
+// by iterative method. 
+#include<iostream> 
+#include<vector> 
+using namespace std; 
+  
+int main() 
+{ 
+    while (1)
+    {
+        while (1)
+        {
+            cout<<"ok";
+            break;
+        }
+        cout<<"break done!";
+        
+    }
+    
+
+    return 0; 
+} 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+	// vector<int> original2 = {1, 4, 3, 2 };
+	// vector<int> desired2 = {1, 2 , 4 ,3};
+    // cout << minPieces(original2, desired2) << endl;
