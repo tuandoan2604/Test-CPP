1ee51c9d2ad3861c52fc6124d2f8d5f7603bd7d2
NguyenSon99 <129094992+NguyenSon99@users.noreply.github.com>
2023-04-05 18:21:51 +0700

    Add files via upload

---
+++ Bai9_WindowManager.cpp
@@ -0,0 +1,57 @@
+
+
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+class WindowManager
+{
+    list <string> nameWindow;
+
+public:
+    void open(const std::string windowName)
+    {
+        bool check =true;
+        
+        for( auto conTro =nameWindow.begin(); conTro!= nameWindow.end(); ++conTro){
+            
+            if(*conTro == windowName){
+                nameWindow.erase(conTro);
+                nameWindow.push_back(windowName);
+                check=false;
+            }
+            
+        }
+        if(check){
+            nameWindow.push_back(windowName);
+        }
+    }
+
+    void close(const std::string windowName)
+    {
+        nameWindow.remove(windowName);
+    }
+
+    std::string getTopWindow()
+    {
+        return nameWindow.back();
+    }
+};
+
+//#ifndef RunTests
+int main()
+{
+    WindowManager wm;
+    wm.open("Calculator");
+    wm.open("Browser");
+    wm.open("Player");
+    wm.open("Browser");
+    wm.close("Player");
+    wm.close("Browser");
+    std::cout << wm.getTopWindow();
+}
+//#endif
+
+
+
