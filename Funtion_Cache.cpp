f9fb1edf7ed15bd6f983578117ae3af5b7007587
Đức Ngọc <tk632k@gmail.com>
2023-04-06 11:54:51 +0700

    Them bai 6

--- Funtion_Cache.cpp
+++ Funtion_Cache.cpp
@@ -1,62 +1,89 @@
-// #include <iostream>
-// #include <unordered_map>
-// #include <memory>
-// #include <functional>
+#include <iostream>
+#include <unordered_map>
+#include <memory>
+#include <functional>
 
-// class FunctionCache
-// {
-//     class Parameters
-//     {
-//     public:
-//         Parameters(int first, int second)
-//         {
-//             this->first = first;
-//             this->second = second;
-//         }
+using namespace std;
 
-//         int first;
-//         int second;
-//     };
+class FunctionCache
+{
+    class Parameters
+    {
+    public:
+        Parameters(int first, int second)
+        {
+            this->first = first;
+            this->second = second;
+        }
 
-// public:
-//     FunctionCache(std::function<int(int, int)> function): function(function) {}
+        int first;
+        int second;
+        
+        // Custom hash function for Parameters
+        struct Hash
+        {
+            size_t operator()(const Parameters& params) const
+            {
+                size_t h1 = std::hash<int>{}(params.first);
+                size_t h2 = std::hash<int>{}(params.second);
+                return h1 ^ h2;
+            }
+        };
 
-//     int calculate(int first, int second)
-//     {
-//         auto args = make_shared<Parameters>(first, second);
-//         auto it = calculations.find(args);
-//         if (it != calculations.end())
-//             return it->second;
+        // Custom equality function for Parameters
+        struct EqualTo
+        {
+            bool operator()(const Parameters& params1, const Parameters& params2) const
+            {
+                return params1.first == params2.first && params1.second == params2.second;
+            }
+        };    
+    
+    };
 
-//         int calculation = function(first, second);
-//         calculations[args] = calculation;
-//         return calculation;
-//     }
+public:
+    FunctionCache(std::function<int(int, int)> function): function(function) {}
 
-// private:
-//     std::unordered_map<std::shared_ptr<Parameters>, int> calculations;
-//     std::function<int(int, int)> function;
-// };
+    int calculate(int first, int second)
+    {
+        Parameters args(first, second);
+        // Hàm find tìm kiếm 1 phần tử trong map có key là args
+        // Nếu tìm thấy  trả về một iterator chỉ đến phần tử đó
+        // Không tìm thấy  trả về iterator trỏ đến phần tử tiếp theo sau phần tử cuối cùng của map
+        auto it = calculations.find(args);
+        // Nếu  tìm thấy
+        if (it != calculations.end())
+            return it->second;
 
-// #ifndef RunTests
+        int calculation = function(first, second);
+        calculations[args] = calculation;
+        return calculation;
+    }
 
-// int modulo(int a, int b)
-// {
-//     std::cout << "Function modulo has been called.\n";
-//     return a % b;
-// }
+private:
+    std::unordered_map<Parameters, int, Parameters::Hash, Parameters::EqualTo> calculations;
+    std::function<int(int, int)> function;
+};
 
-// int main()
-// {
-//     FunctionCache cache(modulo);
+#ifndef RunTests
 
-//     // Function modulo should be called.
-//     std::cout << cache.calculate(5, 2) << std::endl;
+int modulo(int a, int b)
+{
+    std::cout << "Function modulo has been called.\n";
+    return a % b;
+}
 
-//     // Function modulo should be called.
-//     std::cout << cache.calculate(7, 4) << std::endl;
+int main()
+{
+    FunctionCache cache(modulo);
 
-//     // Function modulo shouldn't be called because we have already made a call with arguments (5, 2)!
-//     std::cout << cache.calculate(5, 2) << std::endl;
-// }
-// #endif
+    // Function modulo should be called.
+    std::cout << cache.calculate(5, 2) << std::endl;
+
+    // Function modulo should be called.
+    std::cout << cache.calculate(7, 4) << std::endl;
+
+    // Function modulo shouldn't be called because we have already made a call with arguments (5, 2)!
+    std::cout << cache.calculate(5, 2) << std::endl;
+}
+#endif
--- a.cpp
+++ a.cpp
@@ -1,89 +1,84 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-using namespace std;
+#include <unordered_map>
+#include <memory>
+#include <functional>
 
 
-int getMinimumConnections(std::vector<std::vector<bool>>& matrix) {
-    int n = matrix.size();
-    std::vector<int> parent(n);
-    for (int i = 0; i < n; i++) {
-        parent[i] = i;
-    }
+class FunctionCache
+{
+    class Parameters
+    {
+    public:
+        Parameters(int first, int second)
+        {
+            this->first = first;
+            this->second = second;
+        }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (matrix[i][j]) {
-                int parent_i = parent[i];
-                int parent_j = parent[j];
-                if (parent_i != parent_j) {
-                    parent[parent_j] = parent_i;
-                }
+        int first;
+        int second;
+
+        // Custom hash function for Parameters
+        struct Hash
+        {
+            size_t operator()(const Parameters& params) const
+            {
+                size_t h1 = std::hash<int>{}(params.first);
+                size_t h2 = std::hash<int>{}(params.second);
+                return h1 ^ h2;
             }
-        }
+        };
+
+        // Custom equality function for Parameters
+        struct EqualTo
+        {
+            bool operator()(const Parameters& params1, const Parameters& params2) const
+            {
+                return params1.first == params2.first && params1.second == params2.second;
+            }
+        };
+    };
+
+public:
+    FunctionCache(std::function<int(int, int)> function) : function(function) {}
+
+    int calculate(int first, int second)
+    {
+        Parameters args(first, second);
+        auto it = calculations.find(args);
+        if (it != calculations.end())
+            return it->second;
+
+        int calculation = function(first, second);
+        calculations[args] = calculation;
+        return calculation;
     }
 
-    int numConnections = 0;
-    for (int i = 0; i < n; i++) {
-        if (parent[i] == i) {
-            numConnections++;
-        }
-    }
-
-    return numConnections - 1;
-}
+private:
+    std::unordered_map<Parameters, int, Parameters::Hash, Parameters::EqualTo> calculations;
+    std::function<int(int, int)> function;
+};
 
 #ifndef RunTests
+
+int modulo(int a, int b)
+{
+    std::cout << "Function modulo has been called.\n";
+    return a % b;
+}
+
 int main()
 {
-    vector<vector<bool>> matrix {
-        {false, true, false, false, true},
-        {true, false, false, false, false},
-        {false, false, false, true, false},
-        {false, false, true, false, false},
-        {true, false, false, false, false}
-    };
+    FunctionCache cache(modulo);
 
-    vector<vector<bool>> all_connected_matrix {
-        {false, true, true, true, true},
-        {true, false, true, true, true},
-        {true, true, false, true, true},
-        {true, true, true, false, true},
-        {true, true, true, true, false}
-    };
+    // Function modulo should be called.
+    std::cout << cache.calculate(5, 2) << std::endl;
 
-    vector<vector<bool>> large_matrix {
-        {false, false, true, false, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {true, false, false, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {true, true, true, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {true, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, true},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, true, true, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, true, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, true, false, false, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, true, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, true, false, false},
-        {false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false}
-    };
+    // Function modulo should be called.
+    std::cout << cache.calculate(7, 4) << std::endl;
 
-    cout << getMinimumConnections(matrix) << endl;                  // should print 1
-    cout << getMinimumConnections(all_connected_matrix) << endl;    // should print 0
-    cout << getMinimumConnections(large_matrix) << endl;            // should print 7
+    // Function modulo shouldn't be called because we have already made a call with arguments (5, 2)!
+    std::cout << cache.calculate(5, 2) << std::endl;
 }
+
 #endif
