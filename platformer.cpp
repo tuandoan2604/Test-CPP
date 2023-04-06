a46b857c2ea8b54e2c8f22e1063d7af4bc41a72c
hieutrung255 <hieutrung.nt255@gmail.com>
2023-04-06 15:05:06 +0700

    bai 6

---
+++ platformer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+class Platformer{
+    private: 
+        int n;
+        int position;
+    public:
+        Platformer(int n, int position){
+            if(n<0 || position<0 || position>n){
+                throw std::logic_error("n va postion phai lon hon hoac bang 0!");
+            }
+            this->n = n;
+            this->position = position;
+        }
+        void jumpLeft(){
+            if(this->position==0){
+                cout<<"can't jump";
+            }else{
+                position -= 1;
+            }
+        }
+        void jumpRight(){
+            if(this->position==this->n){
+                cout<<"cant jump";
+            }else{
+                position+=1;
+            }
+        }
+        int position(){
+            return this->position;
+        }
+};
+
